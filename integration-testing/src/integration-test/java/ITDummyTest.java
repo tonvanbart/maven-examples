bb8e694f0e58fe7926b21f0849e9c927d81c8c0e
Ton van Bart <ton.vanbart@kpn.com>
2018-12-24 10:35:47 +0100

    added logging

--- integration-testing/src/integration-test/java/ITDummyTest.java
+++ integration-testing/src/integration-test/java/ITDummyTest.java
@@ -1,4 +1,6 @@
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static junit.framework.Assert.assertTrue;
 
@@ -7,8 +9,11 @@ import static junit.framework.Assert.assertTrue;
  */
 public class ITDummyTest {
 
+    private static final Logger log = LoggerFactory.getLogger(ITDummyTest.class);
+
     @Test
     public void dummyTest() {
+        log.info("dummyTest()");
         assertTrue(true);
     }
 }
